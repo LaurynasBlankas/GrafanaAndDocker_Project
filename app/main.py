42d54fe96e4ffa973116f67807f71227d169ee9f
Laurynas Blankas <laurynas.blankas@gmail.com>
2026-06-09 14:07:10 +0300

    added error endpoint and a way to watch in Grafana

--- app/main.py
+++ app/main.py
@@ -6,11 +6,14 @@ app = FastAPI()
 
 REQUEST_COUNT = Counter("app_requests_total", "Total requests", ["endpoint"])
 REQUEST_LATENCY = Histogram("app_request_latency_seconds", "Latency", ["endpoint"])
+ERROR_COUNT = Counter("app_errors_total", "Total errors", ["endpoint"])
+
 
 @app.get("/")
 def root():
     REQUEST_COUNT.labels(endpoint="/").inc()
     return {"message": "Hello from Docker!"}
+
 
 @app.get("/slow")
 def slow():
@@ -18,6 +21,20 @@ def slow():
     time.sleep(0.5)
     REQUEST_LATENCY.labels(endpoint="/slow").observe(time.time() - start)
     return {"message": "That was slow"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+@app.get("/error")
+def error(response: Response):
+    ERROR_COUNT.labels(endpoint="/error").inc()
+    REQUEST_COUNT.labels(endpoint="/error").inc()
+    response.status_code = 500
+    return {"message": "Something went wrong!"}
+
 
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
